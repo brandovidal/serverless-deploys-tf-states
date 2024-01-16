9db5de562f895a7d8ea47cf2b5c944ea67137e60
Brando Vidal <yisusdracon@gmail.com>
2024-01-16 08:17:59 -0500

    🔨  combine policies

--- src/s3.tf
+++ src/s3.tf
@@ -17,7 +17,7 @@ resource "aws_s3_bucket_public_access_block" "default" {
 
 resource "aws_s3_bucket_policy" "policy_allow_s3" {
   bucket = aws_s3_bucket.tf_states_bucket.id
-  policy = data.aws_iam_policy_document.allow_s3.json
+  policy = data.aws_iam_policy_document.combined.json
 
   depends_on = [aws_s3_bucket_public_access_block.default]
 }
@@ -43,6 +43,38 @@ data "aws_iam_policy_document" "allow_s3" {
       identifiers = ["*"]
     }
   }
+}
+
+data "aws_iam_policy_document" "enforce_tls" {
+  version = "2012-10-17"
+
+  statement {
+    sid    = "EnforceTLSv12orHigher"
+    effect = "Deny"
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject"
+    ]
+    resources = [
+      aws_s3_bucket.tf_states_bucket.arn,
+      "${aws_s3_bucket.tf_states_bucket.arn}/*"
+    ]
+    condition {
+      test     = "NumericLessThan"
+      variable = "s3:TlsVersion"
+      values   = ["1.2"]
+    }
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "combined" {
+  source_policy_documents = [data.aws_iam_policy_document.allow_s3.json, data.aws_iam_policy_document.enforce_tls.json]
 }
 
 resource "aws_s3_bucket_versioning" "versioning_enabled" {
